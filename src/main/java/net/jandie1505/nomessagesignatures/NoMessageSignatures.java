c1fdb90209ce885b2130ff31607190579f1c1fab
jandie1505 <34031551+jandie1505@users.noreply.github.com>
2025-07-26 00:19:58 +0200

    updated to 1.21.7

--- src/main/java/net/jandie1505/nomessagesignatures/NoMessageSignatures.java
+++ src/main/java/net/jandie1505/nomessagesignatures/NoMessageSignatures.java
@@ -17,7 +17,7 @@ import java.lang.reflect.Field;
 import java.util.logging.Level;
 
 public final class NoMessageSignatures extends JavaPlugin {
-    public static final String TARGET_VERSION = "1.21.1-R0.1-SNAPSHOT";
+    public static final String TARGET_VERSION = "1.21.7-R0.1-SNAPSHOT";
     private final Mode packetMode;
     private final ConfigManager configManager;
 
